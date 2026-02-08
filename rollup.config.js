94714e1c6847a9260ed25bd21e75ae77fe738419
Lucian Pacurar <codesensedev@outlook.com>
2026-02-08 17:34:04 -0600

    rename package, update description and license

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,30 @@ export default {
   output: {
     file: 'dist/js/graal.js',
     format: 'esm',
-    sourcemap: true
+    sourcemap: true,
+    banner: `/*!
+ * Graal - Minimalist library for building complex responsive layouts
+ *
+ * Copyright (c) 2025 Lucian Pacurar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */`
   },
   plugins: [
     resolve(),
@@ -48,7 +71,7 @@ export default {
     terser({
       ecma: 2020,
       format: {
-        comments: false
+        comments: /^!/  // Preserve only /*! ... */ license comments (see also 'some')
       },
       compress: {
         drop_console: false,
