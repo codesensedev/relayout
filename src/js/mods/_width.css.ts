7229f53909a160423448f3b43da2563b2bcec527
Lucian Pacurar <codesensedev@outlook.com>
2026-02-17 19:16:29 -0600

    fix mod width

--- src/js/mods/_width.css.ts
+++ src/js/mods/_width.css.ts
@@ -4,10 +4,10 @@ import { css } from 'lit';
 
 // .rl-width-*
 export const _widthCss = css`
-    :host([rl-width-xs]) { max-width: var(--rl-width-xs); margin: 0 auto; }
-    :host([rl-width-s]) { max-width: var(--rl-width-s); margin: 0 auto; }
-    :host([rl-width-m]) { max-width: var(--rl-width-m); margin: 0 auto; }
-    :host([rl-width-l]) { max-width: var(--rl-width-l); margin: 0 auto; }
-    :host([rl-width-xl]) { max-width: var(--rl-width-xl); margin: 0 auto; }
-    :host([rl-width-2xl]) { max-width: var(--rl-width-2xl); margin: 0 auto; }
+    :host([rl-width-xs]) { max-width: var(--rl-width-xs); margin: 0 auto; width: 100%; }
+    :host([rl-width-s]) { max-width: var(--rl-width-s); margin: 0 auto; width: 100%; }
+    :host([rl-width-m]) { max-width: var(--rl-width-m); margin: 0 auto; width: 100%; }
+    :host([rl-width-l]) { max-width: var(--rl-width-l); margin: 0 auto; width: 100%; }
+    :host([rl-width-xl]) { max-width: var(--rl-width-xl); margin: 0 auto; width: 100%; }
+    :host([rl-width-2xl]) { max-width: var(--rl-width-2xl); margin: 0 auto; width: 100%; }
 `;
