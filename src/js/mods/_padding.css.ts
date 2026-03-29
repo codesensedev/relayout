1bfbd0e95d064ce2ffbf2a09dfb2c9781ccc4f16
Lucian Pacurar <codesensedev@outlook.com>
2026-03-29 09:16:53 -0500

    add padding for horizontal and vertical

--- src/js/mods/_padding.css.ts
+++ src/js/mods/_padding.css.ts
@@ -11,4 +11,20 @@ export const _paddingCss = css`
     :host([rl-padding-l]) { padding: var(--rl-space-l); }
     :host([rl-padding-xl]) { padding: var(--rl-space-xl); }
     :host([rl-padding-2xl]) { padding: var(--rl-space-2xl); }
+
+    :host([rl-padding-x-0]) { padding-inline: var(--rl-space-0); }
+    :host([rl-padding-x-xs]) { padding-inline: var(--rl-space-xs); }
+    :host([rl-padding-x-s]) { padding-inline: var(--rl-space-s); }
+    :host([rl-padding-x-m]) { padding-inline: var(--rl-space-m); }
+    :host([rl-padding-x-l]) { padding-inline: var(--rl-space-l); }
+    :host([rl-padding-x-xl]) { padding-inline: var(--rl-space-xl); }
+    :host([rl-padding-x-2xl]) { padding-inline: var(--rl-space-2xl); }
+
+    :host([rl-padding-y-0]) { padding-block: var(--rl-space-0); }
+    :host([rl-padding-y-xs]) { padding-block: var(--rl-space-xs); }
+    :host([rl-padding-y-s]) { padding-block: var(--rl-space-s); }
+    :host([rl-padding-y-m]) { padding-block: var(--rl-space-m); }
+    :host([rl-padding-y-l]) { padding-block: var(--rl-space-l); }
+    :host([rl-padding-y-xl]) { padding-block: var(--rl-space-xl); }
+    :host([rl-padding-y-2xl]) { padding-block: var(--rl-space-2xl); }
 `;
